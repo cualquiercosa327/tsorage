e5c797384ba736cdd2640a2499a2974eb2c134dd
Mathieu Goeminne <mathieu.goeminne@cetic.be>
2019-10-02 11:15:29 +0200

    Remove functions for submitting temporally aggregated values, since this kind of value is now managed as any raw or aggregated value by using UDTs.

--- src/main/scala/be/cetic/tsorage/processor/DAO.scala
+++ src/main/scala/be/cetic/tsorage/processor/DAO.scala
@@ -6,14 +6,12 @@ import com.typesafe.scalalogging.LazyLogging
 
 object DAO extends LazyLogging with TimeFormatHelper
 {
-
-
    def colName(name: String) = s""""$name""""
    def colValue(value: String)= s"""'${value.replace("'", "''")}'"""
    def tagAsClause(key: String, value: String) = s"(${colName(key)} = ${colValue(value)} )"
 
-   private val rawKeyspace = ProcessorConfig.conf.getString("cassandra.keyspaces.raw")
-   private val aggKeyspace = ProcessorConfig.conf.getString("cassandra.keyspaces.aggregated")
+   private val rawKeyspace = ProcessorConfig.rawKS
+   private val aggKeyspace = ProcessorConfig.aggKS
 
    def tagsetAsClause(tagset: Map[String, String]) = tagset match {
       case m:Map[String, String] if m.isEmpty => ""
@@ -101,51 +99,6 @@ object DAO extends LazyLogging with TimeFormatHelper
             |   ${tagsetAsClause(tagset)}
             |
            | ALLOW FILTERING;
-         """.stripMargin
-
-      query
-   }
-
-   /**
-     * Creates the query for retrieving the aggregated temporal values corresponding of a shunk, over a specified shard
-     * @param metric    The metric characterizing the time series
-     * @param shard     The specified shard
-     * @param interval  The aggregation interval to retrieve
-     * @param aggregator   The aggregation function to use
-     * @param shunkStart   The lower bound of the shunk)
-     * @param shunkEnd     The upper bound of the shunk
-     * @param tagset       The tagset characterizing the time series
-     * @return A string representing a CQL query corresponding to the specified shunk
-     */
-   def getAggTempShunkValues(
-                           metric: String,
-                           shard: String,
-                           interval: String,
-                           aggregator: String,
-                           shunkStart: LocalDateTime,
-                           shunkEnd: LocalDateTime,
-                           tagset: Map[String, String]
-                        ) =
-   {
-      /*
-       * TODO : Datastax Query Builder replaces any string variable by "?" instead of the actual string value.
-       *  Investigate and use the builder instead of this function.
-       */
-
-      val query =
-         s"""
-            | SELECT observation_datetime_, value_double_
-            | FROM ${aggKeyspace}.numeric
-            | WHERE
-            |   (metric_ = '${metric}') AND
-            |   (shard_ = '${shard}') AND
-            |   (interval_ = '${interval}') AND
-            |   (aggregator_ = '${aggregator}') AND
-            |   (datetime_ > '${formatLDT(shunkStart)}') AND
-            |   (datetime_ <= '${formatLDT(shunkEnd)}')
-            |   ${tagsetAsClause(tagset)}
-            |
-            | ALLOW FILTERING;
          """.stripMargin
 
       query
