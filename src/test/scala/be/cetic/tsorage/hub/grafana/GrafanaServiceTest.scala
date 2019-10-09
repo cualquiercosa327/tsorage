969c20fe5c58121418f30ba465fb420819e8e92f
Nico Salamone <nico.salamone@cetic.be>
2019-10-09 15:35:17 +0200

    Add the search route test

--- src/test/scala/be/cetic/tsorage/hub/grafana/GrafanaServiceTest.scala
+++ src/test/scala/be/cetic/tsorage/hub/grafana/GrafanaServiceTest.scala
@@ -1,9 +1,29 @@
 package be.cetic.tsorage.hub.grafana
 
-import org.scalatest.FunSuite
+import akka.http.scaladsl.model.MediaTypes.`application/json`
+import akka.http.scaladsl.model.{HttpEntity, StatusCodes}
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import be.cetic.tsorage.hub.grafana.jsonsupport.{GrafanaJsonSupport, SearchResponse}
+import org.scalatest.{Matchers, WordSpec}
 
-class GrafanaServiceTest extends FunSuite{
-  test("mytest") {
-    assert(true)
+class GrafanaServiceTest extends WordSpec with Matchers with ScalatestRouteTest with GrafanaJsonSupport {
+  val database: Database = FakeDatabase
+  val grafanaService = new GrafanaService(database)
+  val getMetricNamesRoute: Route = grafanaService.getMetricNamesRoute
+  val postMetricNamesRoute: Route = grafanaService.postMetricNamesRoute
+  val postQueryRoute: Route = grafanaService.postQueryRoute
+  val postAnnotationRoute: Route = grafanaService.postAnnotationRoute
+
+  "The Grafana service" should { // TODO: change name of this string?
+    // Search route.
+    "return the name of all metrics for GET/POST requests to the search path" in {
+      Get("/search") ~> getMetricNamesRoute ~> check {
+        responseAs[SearchResponse].targets.toSet shouldEqual database.metrics.toSet
+      }
+      Post("/search", HttpEntity(`application/json`, "{}")) ~> postMetricNamesRoute ~> check {
+        responseAs[SearchResponse].targets.toSet shouldEqual database.metrics.toSet
+      }
+    }
   }
 }
