0994f2824b7d5bb697fe0b938eeb8bb78035d045
Nico Salamone <nico.salamone@cetic.be>
2019-10-09 16:57:27 +0200

    Add the annotation route test

--- src/test/scala/be/cetic/tsorage/hub/grafana/GrafanaServiceTest.scala
+++ src/test/scala/be/cetic/tsorage/hub/grafana/GrafanaServiceTest.scala
@@ -1,11 +1,13 @@
 package be.cetic.tsorage.hub.grafana
 
+import be.cetic.tsorage.hub.grafana.jsonsupport.{Annotation, AnnotationRequest, AnnotationResponse, GrafanaJsonSupport, SearchRequest, SearchResponse}
 import akka.http.scaladsl.model.MediaTypes.`application/json`
 import akka.http.scaladsl.model.{HttpEntity, StatusCodes}
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import be.cetic.tsorage.hub.grafana.jsonsupport.{GrafanaJsonSupport, SearchResponse}
-import org.scalatest.{Matchers, WordSpec}
+import spray.json._
+import org.scalatest.{Matchers, MustMatchers, WordSpec}
+
 
 class GrafanaServiceTest extends WordSpec with Matchers with ScalatestRouteTest with GrafanaJsonSupport {
   val database: Database = FakeDatabase
@@ -15,14 +17,34 @@ class GrafanaServiceTest extends WordSpec with Matchers with ScalatestRouteTest 
   val postQueryRoute: Route = grafanaService.postQueryRoute
   val postAnnotationRoute: Route = grafanaService.postAnnotationRoute
 
-  "The Grafana service" should { // TODO: change name of this string?
+  "The Grafana service" should {
     // Search route.
     "return the name of all metrics for GET/POST requests to the search path" in {
       Get("/search") ~> getMetricNamesRoute ~> check {
-        responseAs[SearchResponse].targets.toSet shouldEqual database.metrics.toSet
+        val response = responseAs[SearchResponse]
+
+        response.targets.toSet shouldEqual database.metrics.toSet
       }
-      Post("/search", HttpEntity(`application/json`, "{}")) ~> postMetricNamesRoute ~> check {
-        responseAs[SearchResponse].targets.toSet shouldEqual database.metrics.toSet
+
+      val request = SearchRequest(Some("Temperature")).toJson.toString
+      Post("/search", HttpEntity(`application/json`, request)) ~> postMetricNamesRoute ~> check {
+        val response = responseAs[SearchResponse]
+
+        response.targets.toSet shouldEqual database.metrics.toSet
+      }
+    }
+
+    // Annotation route.
+    "return an annotation for POST requests to the annotation path" in {
+      val annotation = Annotation("my_annotation", enable = true, "SimpleJson", Some("rgba(255, 96, 96, 1)"), None)
+      val request = AnnotationRequest(annotation).toJson.toString
+      Post("/annotations", HttpEntity(`application/json`, request)) ~> postAnnotationRoute ~> check {
+        val response = responseAs[AnnotationResponse]
+
+        response.annotations.head.annotation shouldEqual annotation
+
+        val currentTime = System.currentTimeMillis
+        response.annotations.head.time shouldEqual currentTime +- 60000 // Tolerance of one minute.
       }
     }
   }
