8da8a8c2900b58976ce7d5f3c9accf8c8526152c
Mohammed Hakawati <mshakawati@hotmail.com>
2019-12-02 10:25:30 +0100

    Using Codec class to encoded and decoded json messages. -accept LocalDataTime. -accept Json values payload.

---
+++ common/src/main/scala/be/cetic/tsorage/common/messaging/Message.scala
@@ -0,0 +1,65 @@
+package be.cetic.tsorage.common.messaging
+
+
+import java.time.LocalDateTime
+
+import be.cetic.tsorage.common.json.MessageJsonSupport
+import spray.json.{DeserializationException, JsArray, JsObject, JsString, JsValue, RootJsonFormat, _}
+
+/**
+ * A package message containing observations.
+ */
+case class Message(
+                    metric: String,
+                    tagset: Map[String, String],
+                    `type`: String,
+                    values: List[(LocalDateTime, JsValue)]
+                  ) extends Serializable
+
+object Message extends MessageJsonSupport
+{
+  implicit def messageFormat = new RootJsonFormat[Message] {
+    def write(msg: Message) = new JsObject(Map(
+      "metric" -> msg.metric.toJson,
+      "tagset" -> msg.tagset.toJson,
+      "type" -> msg.`type`.toJson,
+      "values" -> msg.values.toJson
+    ))
+
+    def read(value: JsValue): Message = value match {
+      case JsObject(fields) => {
+        val metric = fields("metric") match {
+          case JsString(x) => x
+          case _ => throw DeserializationException("String expected as metric.")
+        }
+
+        val tagset = fields.get("tagset") match {
+          case Some(JsObject(x)) => x.mapValues(value => value match { case JsString(s) => s})
+          case None => Map.empty[String, String]
+          case _ => throw DeserializationException("Dictionary expected as tagset.")
+        }
+
+        val `type` = fields("type") match {
+          case JsString(t) => t
+          case _ => throw DeserializationException("String expected as type.")
+        }
+
+
+        val values = (fields("values") match {
+            case JsArray(observations) => {
+              observations.map(obs => obs match {
+                case JsArray(element) => ( element(0).convertTo[LocalDateTime] , element(1))
+                case _ => throw DeserializationException(s"Invalid values.")
+              })
+            }
+
+          case _ => throw DeserializationException("Array expected values.")
+        }).toList
+
+        Message(metric, tagset, `type`, values)
+      }
+
+      case _ => throw DeserializationException(s"Message expected; got ${value.compactPrint} instead.")
+    }
+  }
+}
---
+++ ingestion/src/main/scala/be/cetic/tsorage/ingestion/producer/Producer1.scala
@@ -0,0 +1,63 @@
+
+
+package be.cetic.tsorage.ingestion.producer
+//package scalapb
+import com.google.protobuf.util.raw2.{Raw2Proto, SENSOR2}
+import java.time.LocalDateTime
+import java.time._
+import java.time.temporal.ChronoField
+import com.google.protobuf.timestamp._
+import spray.json.DefaultJsonProtocol._
+import be.cetic.tsorage.common.codec.Codec
+import be.cetic.tsorage.common.messaging.Message
+import akka.actor.ActorSystem
+import akka.kafka.ProducerSettings
+import akka.serialization.SerializationExtension
+import akka.stream.ActorMaterializer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.{ByteArraySerializer, StringSerializer}
+import spray.json._
+import java.time.ZoneOffset
+object Producer1 {
+
+  implicit val system = ActorSystem("http-interface")
+  val serialization = SerializationExtension(system)
+  implicit val materializer = ActorMaterializer()
+  implicit val executionContext = system.dispatcher
+
+  def main(args: Array[String]): Unit = {
+
+    val producerSettings = ProducerSettings(system, new StringSerializer, new ByteArraySerializer)
+      .withBootstrapServers("localhost:9092")
+    //s"${conf.getString("kafka.host")}:${conf.getInt("kafka.port")}"
+    val kafkaProducer = producerSettings.createKafkaProducer()
+    val TOPIC = "test"
+
+    // a Message values for testing the Codec Class
+    var msg = new Message(
+      metric =  "datadog.trace_agent.trace_writer.errors" ,
+      tagset = Map("version"->"6.13.0","type"->"rate","interval"->"10","host"->"macbook-mg.local"),
+      `type` = "rate",
+      values = List((LocalDateTime.now(),{0.77777}.toJson),(LocalDateTime.now(),{0.154215}.toJson))
+    )
+
+
+    var codec = new Codec
+    var a = codec.encode(msg)
+    println("***************************************************************************")
+    println(msg)
+    val record: ProducerRecord[String, Array[Byte]] = new ProducerRecord("test", a)
+    kafkaProducer.send(record)
+
+    println(">>>>>>>" + s"sending $record")
+    kafkaProducer.flush()
+    kafkaProducer.close()
+  }
+}
+
+
+
+
+
+
+
